586cacb7e1f6bec2d13450fda52b6859f0930ac2
3msinhvien <127117652+3msinhvien@users.noreply.github.com>
2025-09-22 08:44:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,1 @@
-<?php
-// web-safe output
-$output = shell_exec('hostname 2>&1'); // 2>&1 để lấy luôn lỗi stderr
-    echo "Hostname: " . htmlspecialchars(trim($output), ENT_QUOTES, 'UTF-8') . "<br>";
-}
+<?PHP echo "Hello THM"; ?>
